81b7664e256e2370fbab5e1d8b5643832bff2924
daniellim922 <daniellim922@gmail.com>
2023-04-26 20:00:37 +0800

    edited env

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const config = require("./config");
 
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 require("dotenv").config();
 
 app.set("view engine", "ejs");
